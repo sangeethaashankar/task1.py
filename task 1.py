14f0476f2e069a393fe63e041e78b6945ef41901
sangeethaashankar <84265905+sangeethaashankar@users.noreply.github.com>
2021-05-22 13:01:22 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+for number in list1:
+    if number>= 0:
+        print(number,end=" ")
+
+
+
+
+list2=[12,14,-95,3]
+for number in list2:
+    if number>= 0:
+        print(number,end="")
+        
